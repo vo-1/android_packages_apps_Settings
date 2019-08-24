6af8005cd18756cb8ddcfcca519e49b12ee87472
Vladimir Chernov <vo-1@yandex.ru>
2019-08-24 15:50:30 +0300

    Implement animations for back edge gestures

--- src/com/lineage/settings/fragments/EdgeGesturesSettings.java
+++ src/com/lineage/settings/fragments/EdgeGesturesSettings.java
@@ -59,7 +59,7 @@ public class EdgeGesturesSettings extends SettingsPreferenceFragment implements
  
     @Override 
     public int getMetricsCategory() { 
-        return MetricsProto.MetricsEvent.LINEAGE_SETTINGS; 
+        return MetricsProto.MetricsEvent.SETTINGS_GESTURES; 
     } 
  
     @Override 
